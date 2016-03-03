65331f2d972f4fce34f6ef987a68be133aeb4e02
乔自朋 <1320280834@qq.com>
2016-03-03 21:34:58 +0800

    三月三号第一天

---
+++ Classes/Article/Controller/ArticleViewController.m
@@ -0,0 +1,41 @@
+//
+//  ArticleViewController.m
+//  One
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 乔自朋. All rights reserved.
+//
+
+#import "ArticleViewController.h"
+
+@interface ArticleViewController ()
+
+@end
+
+@implementation ArticleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    self.title = @"One";
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
